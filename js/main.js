02e8e33d0a1d19127c8a6eec144b97776cab56a7
nhathongthai <youremail@gmail.com>
2021-03-09 21:56:51 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+let slidePosition = 0;
+const slides = document.getElementsByClassName("carousel__item");
+const totalSlides = slides.length;
+document
+  .getElementById("carousel__button--next")
+  .addEventListener("click", function () {
+    moveToNextSlide();
+  });
+document
+  .getElementById("carousel__button--prev")
+  .addEventListener("click", function () {
+    moveToPrevSlide();
+  });
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove("carousel__item--visible");
+    slide.classList.add("carousel__item--hidden");
+  }
+  slides[slidePosition].classList.add("carousel__item--visible");
+}
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+  updateSlidePosition();
+}
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+  updateSlidePosition();
+}
+
+// changecolor o day .
+
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const color3 = document.querySelector(".color3");
+const color4 = document.querySelector(".color4");
+const color5 = document.querySelector(".color5");
+const img = document.querySelector("#img-color");
+//bắt sự kiện click
+color1.addEventListener("click", function () {
+  img.src = "./images/choose1.jpg";
+});
+color2.addEventListener("click", function () {
+  img.src = "./images/choose2.jpg";
+});
+color3.addEventListener("click", function () {
+  img.src = "./images/choose3.jpg";
+});
+color4.addEventListener("click", function () {
+  img.src = "./images/choose4.jpg";
+});
+color5.addEventListener("click", function () {
+  img.src = "./images/choose5.jpg";
+});
